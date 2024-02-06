bdab099e6afb4744cae15687bc574f52258dffef
Mateus Ferraz <ferraz_dev@proton.me>
2024-02-06 09:33:02 -0300

    Add basic logout and login using states.

--- src/context/AuthProvider/index.tsx
+++ src/context/AuthProvider/index.tsx
@@ -1,18 +1,21 @@
 import React, {createContext, useState} from 'react'
 import { IAuthProvider, IContext, IUser } from './types'
+import { LoginRequest } from './util'
 
 export const AuthContext = createContext<IContext>({} as IContext)
 
 export const AuthProvider = ({ children }: IAuthProvider) => {
     const [user, setUser] = useState<IUser | null>()
 
-    async function authenticate (email: string, password: string) {
-        
-    } 
-    
-    async function logout () {
+    const authenticate = async (email: string, password: string) => {
+        const response = await LoginRequest(email, password)
 
+        const payload = { token: response.token, email }
+
+        setUser(payload)
     } 
+
+    const logout = async () => setUser(null)
 
     return (
         <AuthContext.Provider value={{...user, authenticate, logout}}>
