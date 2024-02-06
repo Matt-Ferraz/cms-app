b9e1c07c851447d62f6a7045a29c281ee5ab0dcc
Mateus Ferraz <ferraz_dev@proton.me>
2024-02-06 09:48:19 -0300

    Add functions to store user on local storage

--- src/context/AuthProvider/util.ts
+++ src/context/AuthProvider/util.ts
@@ -1,6 +1,21 @@
 import { Api } from '@/services/Api'
+import { IUser } from './types'
 
-export async function LoginRequest (email: string, password: string) {
+export function setUserLocalStorage(user: IUser) {
+    localStorage.setItem('usr', JSON.stringify(user))
+} 
+
+export function getUserLocalStorage() {
+    const userStr = localStorage.getItem('usr')
+
+    if (!userStr) return null
+
+    const user = JSON.parse(userStr)
+
+    return user ?? null
+} 
+
+export const LoginRequest = async (email: string, password: string) => {
     try {
         const requestLogin = await Api.post('login', { email, password })
 
